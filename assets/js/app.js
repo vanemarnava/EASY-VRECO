ff144e1c7e4aceede2b51bab5570cd93d5a4a21c
carol-solivos <carol.solivos@gmail.com>
2018-02-06 11:14:36 -0400

    detecta latitud y longitud

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+let x = document.getElementById("ubicacion");
+function myUbicacion() {
+        navigator.geolocation.getCurrentPosition(showPosition);
+}
+function showPosition(position) {
+    x.innerHTML = "Latitud: " + position.coords.latitude + 
+                  "<br>Longitud: " + position.coords.longitude;
+}
